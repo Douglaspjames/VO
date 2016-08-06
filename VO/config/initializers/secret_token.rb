d558e28ab072b64100bb3bb2f571b2d9e8f64e3b
Eleanor RooseveltDoug <dpjames29@gmail.com>
2016-08-06 13:24:18 -0500

    first commit

---
+++ VO/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VO::Application.config.secret_key_base = '67b950587ab52f245be88db5afca62f283a2cf010d9bdb24d2e84ec7548efbeebbe1be6a0ecaf242c28c2adf49cfa68f7b7d7ea92ef027101aae509ea5348684'
